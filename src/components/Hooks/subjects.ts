ab8f32245a0b78252f25484c2ba75605bff8e841
qiaoli116 <qiaoli.116@gmail.com>
2023-11-09 01:15:52 +1100

    update

--- src/components/Hooks/subjects.ts
+++ src/components/Hooks/subjects.ts
@@ -14,4 +14,49 @@ const filterValues = {
       filter[`$${key}`] = { $in: values };
     }
   }
-*/ 
+*/
+
+import { ISubject, ISubjectCreateInput } from "@/types";
+import { type } from "os";
+import { gql, useQuery, useMutation, CombinedError, UseMutationExecute } from "urql";
+
+export interface ICreateSubjectMutationVariables extends ISubjectCreateInput {
+    // tasIndex: ITASIndex;
+    // code: string;
+    // title: string;
+    // term: string;
+    // department: string;
+    // block: string;
+    // qualification: IQualification;
+    // units: ITASUnit[];
+}
+type CreateSubjectMutationData = { subjectCreate: ISubject } | undefined | null;
+export function useCreateSubject(): [UseMutationExecute<CreateSubjectMutationData, ICreateSubjectMutationVariables>] {
+
+    const SUBJECT_CREATE_MUTATION = gql`
+        mutation Mutation($code: String, $title: String, $term: String, $department: String, $block: String, $qualification: QualificationInput, $tasIndex: TASIndexInput, $units: [TASUnitInput]) {
+            subjectCreate(code: $code, title: $title, term: $term, department: $department, block: $block, qualification: $qualification, tasIndex: $tasIndex, units: $units) {
+                block
+                code
+                department
+                qualification {
+                    code
+                    title
+                }
+                term
+                title
+                tasIndex {
+                    department
+                    qualificationCode
+                    year
+                }
+            }
+        }
+    `;
+
+    const [result, executeMutation] = useMutation<CreateSubjectMutationData, ICreateSubjectMutationVariables>(SUBJECT_CREATE_MUTATION);
+
+    return [
+        executeMutation
+    ];
+}
